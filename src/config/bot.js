d861c864e8698763f44dd9f163dd701ba274ac2f
notcorelol <gabingeorges59@gmail.com>
2026-06-06 15:23:17 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Sento | /help | Feared by pepole.",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 1, 
       },
     ],
   },
