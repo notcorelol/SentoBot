a61ba5cd5757d9531a2bf799b9ec0b3015ce02ab
notcorelol <gabingeorges59@gmail.com>
2026-06-07 12:03:22 +0200

    Update support.js

--- src/commands/Core/support.js
+++ src/commands/Core/support.js
@@ -3,7 +3,7 @@ import { createEmbed } from '../../utils/embeds.js';
 import { logger } from '../../utils/logger.js';
 
 import { InteractionHelper } from '../../utils/interactionHelper.js';
-const SUPPORT_SERVER_URL = "https://discord.gg/QnWNz2dKCE";
+const SUPPORT_SERVER_URL = "https://discord.gg/5EK3vKs27A";
 export default {
     data: new SlashCommandBuilder()
     .setName("support")
