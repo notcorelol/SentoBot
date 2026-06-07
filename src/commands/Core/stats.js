d2a02497eb661b97fb5ce922f420bda3de479765
notcorelol <gabingeorges59@gmail.com>
2026-06-07 12:54:00 +0200

    Update stats.js

--- src/commands/Core/stats.js
+++ src/commands/Core/stats.js
@@ -20,7 +20,7 @@ export default {
       const nodeVersion = process.version;
 
       const embed = createEmbed({ title: "<:stats:1513123713711870083> Bot statistics", description: "Bot information\n**Name**: `Sento`\n**ID**: `1512738193932288030`\n **Created**: Saturday, June, 6th\n<a:developer:1513128854841982997> Developer: (c) `corelol.`\n<:github:1513133208210767892> Github: `notcorelol`" }).addFields(
-        { name: "Servers", value: `${totalGuilds}`, inline: true },
+        { name: "<:link:1513123238106890241> Servers", value: `${totalGuilds}`, inline: true },
         { name: "<:user:1513123331782738031> Users", value: `${totalMembers}`, inline: true },
         { name: "<:node:1513122426991415407> Node.js", value: `${nodeVersion}`, inline: true },
         { name: "<:discord:1513122602741403748> Discord.js", value: `v${version}`, inline: true },
